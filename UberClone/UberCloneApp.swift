7361be8af6b999d4b058aedc32bd9826feaa3a8a
Tom Miller <tommymillerr@outlook.com>
2022-12-03 15:31:36 -0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Tom Miller on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
